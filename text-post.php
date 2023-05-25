459e1d45b7d1f18f73bc5f35c591b8539f54291a
raymondlowe <49975086+raymondlowe@users.noreply.github.com>
2023-05-25 09:59:15 +0800

    Add publish or draft

--- text-post.php
+++ text-post.php
@@ -1,8 +1,8 @@
 <?php
 /* 
-Plugin Name: Multi-File Text Upload  
-Description: Allows the user to upload multiple .txt and .md files and create new posts from them.
-Version: 2.1
+Plugin Name: Multi-File Text Upload with Post Status
+Description: Allows the user to upload multiple .txt and .md files and create new posts from them with different post status.
+Version: 2.2
 */
 
 // Add a new admin menu item for the file upload
@@ -11,7 +11,7 @@ function mftu_add_menu_item() {
     'tools.php',
     'Multi-File Text Upload',
     'Multi-File Text Upload',
-    'manage_options',
+   'manage_options',
     'multi-file-text-upload',
     'mftu_handle_upload'
   );  
@@ -52,23 +52,22 @@ function mftu_handle_upload() {
           array_shift( $lines );
         }
 
-foreach ( $lines as $line ) {
-	if ( preg_match( '/^\*+/', $line ) ) {
-		// If the line starts with one or more star symbols, treat it as a heading
-		$text = str_replace( '*', '', $line );
-		$heading_level = min( strlen( $line ) - strlen( ltrim( $line, '*' ) ), 6 );
-		$parsed_content .= '<h' . $heading_level . '>' . $text . '</h' . $heading_level . '>';
-	} elseif ( strpos( $line, '#' ) === 0 ) {
-		// If the line starts with one or more hash symbols, treat it as a heading
-		$heading_level = min( substr_count( $line, '#' ), 6 );
-		$text = trim( str_replace( '#', '', $line ) );
-		$parsed_content .= '<h' . $heading_level . '>' . $text . '</h' . $heading_level . '>';
-	} else {
-		// Otherwise, treat the line as plain text
-		$parsed_content .= $line . "\n";
-	}
-}
-		
+        foreach ( $lines as $line ) {
+          if ( preg_match( '/^\*+/', $line ) ) {
+            // If the line starts with one or more star symbols, treat it as a heading
+            $text = str_replace( '*', '', $line );
+            $heading_level = min( strlen( $line ) - strlen( ltrim( $line, '*' ) ), 6 );
+            $parsed_content .= '<h' . $heading_level . '>' . $text . '</h' . $heading_level . '>';
+          } elseif ( strpos( $line, '#' ) === 0 ) {
+            // If the line starts with one or more hash symbols, treat it as a heading
+            $heading_level = min( substr_count( $line, '#' ), 6 );
+            $text = trim( str_replace( '#', '', $line ) );
+            $parsed_content .= '<h' . $heading_level . '>' . $text . '</h' . $heading_level. '>';
+          } else {
+            // Otherwise, treat the line as plain text
+            $parsed_content .= $line . "\n";
+          }
+        }
 
         // Create a new post for the uploaded file
         $post_title = $headline;
@@ -77,13 +76,14 @@ foreach ( $lines as $line ) {
         // Create a new post for the uploaded file
         $post_category = get_option( 'mftu_category', 0 );
         $post_author = $_POST['author'];
+        $post_status = $_POST['post_status'];
 
         $post_id = wp_insert_post( array(
           'post_title' => $post_title,
           'post_content' => $post_content,
           'post_category' => array( $post_category ),
           'post_author' => $post_author,
-          'post_status' => 'publish'
+          'post_status' => $post_status
         ) );
 
         if ( $post_id ) {
@@ -113,6 +113,13 @@ foreach ( $lines as $line ) {
       <label for="author">Author:</label>
       <?php wp_dropdown_users( array( 'name' => 'author' ) ); ?>
       <br><br> 
+      <label for="post_status">Post Status:</label>
+      <select name="post_status" id="post-status">
+        <option value="draft">Draft</option>
+        <option value="publish">Publish</option>
+        <option value="private">Private</option>
+      </select>
+      <br><br>
       <?php submit_button( 'Upload Files' ); ?>
     </form>
   </div>
